3eea87c687296dff7d96430f438563cdcf53a534
Gregor Cox <gregorcox@gmail.com>
2018-08-28 20:38:27 +0100

    gets data from itunes api

--- src/containers/SongContainer.js
+++ src/containers/SongContainer.js
@@ -10,7 +10,13 @@ class SongContainer extends React.Component {
 
   componentDidMount() {
     const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json'
-    
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((songs) => {
+        this.setState({songs: songs})
+      })
   }
 
 }
